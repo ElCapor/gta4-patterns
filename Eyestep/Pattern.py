ed6d07d208c70ca2f7d5f596a1d70c66f3c00bd0
ElCapor <104172250+ElCapor@users.noreply.github.com>
2024-05-29 16:16:28 +0200

    Find msPedPool

--- Eyestep/Pattern.py
+++ Eyestep/Pattern.py
@@ -40,7 +40,7 @@ def compare_bytes(data, pattern, mask, start):
             return False
     return True
 
-def aob_scan(data, ida_pattern):
+def aob_scan(data :bytes, ida_pattern :str, offset :int = 0):
     pattern, mask = convert_ida_pattern_to_byte_pattern_and_mask(ida_pattern)
     pattern_length = len(pattern)
     data_length = len(data)
@@ -49,9 +49,27 @@ def aob_scan(data, ida_pattern):
 
     for i in range(data_length - pattern_length + 1):
         if compare_bytes(data, pattern, mask, i):
-            matches.append(i)
+            matches.append(i + offset)
 
     return matches
+
+
+def find_pattern(data :bytes, ida_pattern :str, offsets :list[int] = [], deferences :list[int] = []) -> list[int]:
+    # you must specify an offset for each deference even if it's 0
+    
+    # initial scan
+    matches = aob_scan(data, ida_pattern)
+    ret = []
+    for match in matches:
+        if (len(deferences) > 0):
+            for i in range(len(deferences)):
+                match += offsets[i]
+                # careful, the ptr we readin is prolly tied to base address
+                match = read_ptr(data, match)
+        else:
+            match += offsets[0]
+        ret.append(match)
+    return ret
 
 def xref_string(data, string :str) -> list[int]:
     # careful , hardcoded base address
@@ -66,9 +84,15 @@ def xref_string(data, string :str) -> list[int]:
 def read_bytes(data,start, len) ->bytes:
     return data[start:start+len]
 
-def read_int(data, start) -> int:
-    return int(read_bytes(data, start, 4).hex(), 16)
+def read_int(data, start, le :bool = False, signed :bool = False) -> int:
+    return int.from_bytes(read_bytes(data, start, 4), byteorder="little" if le else "big", signed=signed)
 
+# read a ptr, for now same as int
+def read_ptr(data , start) -> int:
+    return read_int(data, start)
+
+def get_relative(data, start) -> int:
+    return start + 5 + read_int(data, start + 1, True, True)
 
 
 """
--- main.py
+++ main.py
@@ -3,10 +3,23 @@ from GtaExecutable import GtaExecutable
 import os
 from Config import Config
 from Eyestep.Eyestep import Eyestep
-from Eyestep.Pattern import read_int,read_bytes,convert_ida_pattern_to_byte_pattern_and_mask, aob_scan, xref_string
+from Eyestep.Pattern import get_relative,read_int,read_bytes,convert_ida_pattern_to_byte_pattern_and_mask, aob_scan, xref_string
 from Eyestep.Utils import d2h, h2d, b2h
 
 config : Config = Config()
+
+
+data = bytearray([
+    0x90, 0x90, 0xE8, 0x01, 0x00, 0x00, 0x00,  # NOP NOP CALL 0x00000001
+    0x90, 0x90, 0xE9, 0x02, 0x00, 0x00, 0x00   # NOP NOP JMP  0x00000002
+])
+start_address = 2  # Position of the CALL instruction
+relative_address = get_relative(data, start_address)
+print(f"Relative address from CALL instruction at {start_address}: {relative_address:#x}")
+
+start_address = 9  # Position of the JMP instruction
+relative_address = get_relative(data, start_address)
+print(f"Relative address from JMP instruction at {start_address}: {relative_address:#x}")
 
 for version in [1200]:
     print(f"====================[VERSION {version} START]=======================")
@@ -18,8 +31,9 @@ for version in [1200]:
     res = read_bytes(eye.data, matches[0], 10)
     print(b2h(res))
     print(read_int(eye.data, matches[0]))
+    print(d2h(get_relative(eye.data, matches[0] + 2) + 0x401000))
 #print(matches)
-#print([d2h(num) for num in matches])
+    print([d2h(num + 0x400000) for num in matches])
 """
     print([d2h(num + gta.get_image_base()) for num in matches])
     for sublist in xref_string(eye.data, "commonimg:/"):
